8dce8be70242d585975d641e3a2c0262f1504103
Andrew Omori <andrewomori7@gmail.com>
2025-09-25 12:00:31 +0300

    adds auth

--- client/src/pages/SignupPage.jsx
+++
@@ -1,9 +0,0 @@
-// src/pages/SignupPage.jsx
-import React from 'react';
-import RegisterForm from '../components/auth/RegisterForm';
-
-const SignupPage = () => {
-  return <RegisterForm />;
-};
-
-export default SignupPage;
