9ea8f2340f41916a11ff19026adfc00cea8c187a
Mkikii <maureenwkarimi@gmail.com>
2025-10-01 22:38:17 +0300

    input logout button

--- client/src/tailwind.config.js
+++ client/src/tailwind.config.js
@@ -1,11 +1,23 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          600: '#9333ea',
+        },
+        brown: {
+          600: '#A0522D',
+          700: '#8B4513',
+        }
+      },
+      fontFamily: {
+        'cursive': ['Dancing Script', 'cursive'],
+      }
+    },
   },
   plugins: [],
 }
