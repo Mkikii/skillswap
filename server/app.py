f0c0f612d184c09dd844f1c3553ed3b91247422b
Mkikii <maureenwkarimi@gmail.com>
2025-10-01 17:16:22 +0300

    Deploy JWT fix to Railway

--- server/app.py
+++ server/app.py
@@ -15,7 +15,7 @@ jwt = JWTManager(app)
 
 @jwt.user_identity_loader
 def user_identity_lookup(user):
-    return str(user.id)
+    return str(user)
 
 @jwt.user_lookup_loader
 def user_lookup_callback(_jwt_header, jwt_data):
