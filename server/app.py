eba453a976297558622f9b4c38a34cbb3de87c13
Mkikii <maureenwkarimi@gmail.com>
2025-10-03 00:42:59 +0300

    Trigger Netlify deployment

--- server/app.py
+++ server/app.py
@@ -59,4 +59,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         print("Database tables created!")
-    app.run(debug=True, port=5555)
+    app.run(debug=True, port=5555)# CORS fix deployment
