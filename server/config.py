f284a841e90024bac4c95de5ece03e9e09703b3e
Mkikii <maureenwkarimi@gmail.com>
2025-10-04 07:27:22 +0300

    fix: Switch to PostgreSQL and force database seeding

--- server/config.py
+++ server/config.py
@@ -1,7 +1,12 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/skillswap.db'
+    if os.environ.get('DATABASE_URL'):
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
+    else:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/skillswap.db'
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'skillswap-secret-key'
     JWT_SECRET_KEY = 'jwt-skillswap-secret'
+
